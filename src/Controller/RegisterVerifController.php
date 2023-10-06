782f0fd1ea98c8c0b4eda78edb68dee08caf8a26
FergusStone <jff.cochard@orange.fr>
2023-10-06 14:08:11 +0200

    <feature>(inscription) : Vérifie que l'email est unique dans la BDD

--- src/Controller/RegisterVerifController.php
+++ src/Controller/RegisterVerifController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-//use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\Utilisateur;
 
 class RegisterVerifController extends AbstractController
@@ -24,32 +23,34 @@ class RegisterVerifController extends AbstractController
             $password2 = $request->request->get('confirm_password');
             $nonadmin = 0;
 
-            if($password == $password2) {
-            // Créez un nouvel utilisateur
-            $user = new Utilisateur();
-            $user->setNomUtilisateur($nom);
-            $user->setPrenomUtilisateur($prenom);
-            $user->setEmailUtilisateur($email);
-            $user->setMotdepasseUtilisateur($password);
-            $user->setAdminUtilisateur($nonadmin);
-            // Encodage du mot de passe
-            //$encodedPassword = $passwordEncoder->encodePassword($user, $motdepasse);
-            //$user->setPassword($encodedPassword);
+            if ($password == $password2) {
+                // Vérifie si l'email existe déjà dans la base de données
+                $userRepository = $this->getDoctrine()->getRepository(Utilisateur::class);
+                $existingUser = $userRepository->findOneBy(['emailUtilisateur' => $email]);
 
-            // Enregistrement de l'utilisateur en base de données
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+                if ($existingUser) {
+                    // L'email existe déjà, renvoie un message d'erreur
+                    return $this->redirectToRoute('app_erreur_register', ['message' => 'Email déjà utilisé']);
+                }
 
-            // Redirection après l'inscription
-            return $this->redirectToRoute('app_accueil'); // Remplacez 'home' par la route de votre choix
-            //redirectToRoute
+                // Créez un nouvel utilisateur
+                $user = new Utilisateur();
+                $user->setNomUtilisateur($nom);
+                $user->setPrenomUtilisateur($prenom);
+                $user->setEmailUtilisateur($email);
+                $user->setMotdepasseUtilisateur($password);
+                $user->setAdminUtilisateur($nonadmin);
 
+                // Enregistrement de l'utilisateur en base de données
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                // Redirection après l'inscription
+                return $this->redirectToRoute('app_accueil'); // Remplacez 'home' par la route de votre choix
+            } else {
+                return $this->redirectToRoute('app_erreur_register', ['message' => 'Les mots de passe ne correspondent pas']);
             }
-            else{
-                return $this->redirectToRoute('app_erreur_register');
-            }
-            
         }
 
         return $this->render('inscription/index.html.twig');
