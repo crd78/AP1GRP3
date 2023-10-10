eac14751198723f9869bfd6d6ac711d787d65c8d
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2023-10-10 17:07:05 +0200

    <feature>(prestation) back de la page (contient des erreur)

--- src/Controller/PrestationsController.php
+++ src/Controller/PrestationsController.php
@@ -1,20 +1,45 @@
-<?php
-
+// src/Controller/PrestationsController.php
 namespace App\Controller;
 
+use App\Entity\prestation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PrestationsController extends AbstractController
 {
     /**
-     * @Route("/prestations", name="app_prestations")
+     * @Route("/prestations", name="prestations")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('prestations/index.html.twig', [
-            'controller_name' => 'PrestationsController',
-        ]);
+        if ($request->isMethod('POST')) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $prestations = $request->request->get('prestations');
+
+            if (!empty($prestations)) {
+                // Créez une entité prestation pour chaque prestation sélectionnée
+                foreach ($prestations as $libellePrestation) {
+                    $prestation = new prestation();
+                    $prestation->setLibellePrestation($libellePrestation);
+                    // Ajoutez d'autres propriétés en conséquence
+
+                    $entityManager->persist($prestation);
+                }
+
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Les données ont été envoyées avec succès.');
+
+                // Rediriger vers une autre page après soumission réussie
+                return $this->redirectToRoute('prestations');
+            } else {
+                $this->addFlash('error', 'Veuillez sélectionner au moins une prestation.');
+            }
+        }
+
+        return $this->render('prestations/index.html.twig');
     }
 }
