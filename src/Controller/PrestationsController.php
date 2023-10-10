70d962b4edff35381e1f1094da39855e1f51dee4
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2023-10-10 17:19:38 +0200

    <feature>(prestation): avancement du back mais toujours des erreurs

--- src/Controller/PrestationsController.php
+++ src/Controller/PrestationsController.php
@@ -1,45 +1,20 @@
-// src/Controller/PrestationsController.php
+<?php
+
 namespace App\Controller;
 
-use App\Entity\prestation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PrestationsController extends AbstractController
+class ContactController extends AbstractController
 {
     /**
-     * @Route("/prestations", name="prestations")
+     * @Route("/prestations", name="app_prestations")
      */
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        if ($request->isMethod('POST')) {
-            $entityManager = $this->getDoctrine()->getManager();
-
-            $prestations = $request->request->get('prestations');
-
-            if (!empty($prestations)) {
-                // Créez une entité prestation pour chaque prestation sélectionnée
-                foreach ($prestations as $libellePrestation) {
-                    $prestation = new prestation();
-                    $prestation->setLibellePrestation($libellePrestation);
-                    // Ajoutez d'autres propriétés en conséquence
-
-                    $entityManager->persist($prestation);
-                }
-
-                $entityManager->flush();
-
-                $this->addFlash('success', 'Les données ont été envoyées avec succès.');
-
-                // Rediriger vers une autre page après soumission réussie
-                return $this->redirectToRoute('prestations');
-            } else {
-                $this->addFlash('error', 'Veuillez sélectionner au moins une prestation.');
-            }
-        }
-
-        return $this->render('prestations/index.html.twig');
+        return $this->render('prestations/index.html.twig', [
+            'controller_name' => 'PrestationsController',
+        ]);
     }
 }
