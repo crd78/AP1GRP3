34c5d81e7b09a7eb0d108ada8ce12b0fba1a7068
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2023-10-11 17:11:41 +0200

    <feature>(prestation) avancer back prestation

--- src/Controller/PrestationsController.php
+++ src/Controller/PrestationsController.php
@@ -1,12 +1,13 @@
 <?php
 
+// src/Controller/PrestationsController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContactController extends AbstractController
+class PrestationsController extends AbstractController
 {
     /**
      * @Route("/prestations", name="app_prestations")
