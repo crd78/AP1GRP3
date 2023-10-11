1c116c6425cc0bbf5bc4ac2d75f1203b23b06689
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2023-10-11 18:30:23 +0200

    <feature>(pretation) avancement du back prestation

--- src/Controller/PrestationsController.php
+++ src/Controller/PrestationsController.php
@@ -1,21 +1,44 @@
 <?php
-
 // src/Controller/PrestationsController.php
 namespace App\Controller;
 
+use App\Entity\Prestation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PrestationsController extends AbstractController
 {
     /**
-     * @Route("/prestations", name="app_prestations")
+     * @Route("/submit.php", name="submit_form", methods={"POST"})
      */
-    public function index(): Response
+    public function submitForm(Request $request): Response
     {
-        return $this->render('prestations/index.html.twig', [
-            'controller_name' => 'PrestationsController',
-        ]);
+        // Récupérer les données du formulaire
+        $prestationsData = $request->request->get('prestations');
+
+        // Créer et enregistrer une nouvelle entité Prestation pour chaque prestation sélectionnée
+        $entityManager = $this->getDoctrine()->getManager();
+
+        foreach ($prestationsData as $prestationName) {
+            $prestation = new Prestation();
+            $prestation->setLibellePrestation($prestationName);
+            $prestation->setPrixUnitairePrestation($prestationName);
+            $prestation->setDescriptionPrestation($prestationName);
+            $prestation->setDureePrestation($prestationName);
+
+
+            $entityManager->persist($prestation);
+        }
+
+        // Enregistrer les changements dans la base de données
+        // Enregistrement de l'utilisateur en base de données
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($prestation);
+        $entityManager->flush();
+
+        // Rediriger ou renvoyer la réponse appropriée
+        return $this->redirectToRoute('app_prestations');
     }
 }
