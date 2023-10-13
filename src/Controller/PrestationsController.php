c04adad89fe90e1a79803e95c74edd36a8084604
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2023-10-13 14:32:01 +0200

    <feature>(prestation) : back page avis fini

--- src/Controller/PrestationsController.php
+++ src/Controller/PrestationsController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Prestation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContactController extends AbstractController
+class PrestationsController extends AbstractController
 {
     /**
      * @Route("/prestations", name="app_prestations")
      */
     public function index(): Response
     {
+        $prestations = $this->getDoctrine()->getRepository(Prestation::class)->findAll();
+
         return $this->render('prestations/index.html.twig', [
-            'controller_name' => 'PrestationsController',
+            'prestations' => $prestations,
         ]);
     }
 }
