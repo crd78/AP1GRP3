266777af5366484bec9f6a8ed83d547fcd9b8f97
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2023-10-13 14:36:27 +0200

    correction faute d ortographe

--- src/Controller/PrestationsController.php
+++ src/Controller/PrestationsController.php
@@ -1,20 +1,26 @@
 <?php
 
+// src/Controller/PrestationsController.php
+
 namespace App\Controller;
 
+use App\Entity\Prestation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PrestationsController extends AbstractController
 {
     /**
-     * @Route("/prestations", name="app_prestations")
+     * @Route("/prestations", name="app_prestation")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $prestations = $this->getDoctrine()->getRepository(Prestation::class)->findAll();
+
         return $this->render('prestations/index.html.twig', [
-            'controller_name' => 'PrestationsController',
+            'prestations' => $prestations,
         ]);
     }
 }
