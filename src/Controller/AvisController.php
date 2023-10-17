d520c87560d072d56bd9f265fde342134265d6d4
FergusStone <jff.cochard@orange.fr>
2023-10-17 16:39:06 +0200

    <feature>(avis) : affichage de tous les avis dans la page avis

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -3,16 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Avis;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AvisController extends AbstractController
 {
     /**
      * @Route("/avis", name="app_avis")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $avis = $this->getDoctrine()->getRepository(Avis::class)->findAll();
+        $avisAvecUtilisateurs = array();
+
+    foreach ($avis as $unavis) {
+    $userId = $unavis->getIdUtilisateurAvis();
+    $userRepository = $entityManager->getRepository(User::class);
+    $unutilisateur = $userRepository->find($userId);
+    $avisAvecUtilisateurs[] = [
+        'avis' => $unavis,
+        'utilisateur' => $unutilisateur,
+    ];
+}
         // Récupérez l'utilisateur connecté
         $user = $this->getUser();
         // Vérifiez si l'utilisateur est connecté
@@ -25,10 +41,12 @@ class AvisController extends AbstractController
                 'controller_name' => 'AvisController',
                 'nom' => $nomUtilisateur,
                 'prenom' => $prenomUtilisateur,
+                'avisAvecUtilisateurs' => $avisAvecUtilisateurs,
             ]);
         } else {
             return $this->render('avis/erreur.html.twig', [
                 'controller_name' => 'AvisController',
+                'avisAvecUtilisateurs' => $avisAvecUtilisateurs,
             ]);
         }
     }
